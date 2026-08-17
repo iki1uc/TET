53addcb6fb8d1b1750bf73745752dcd2cfbb8f77
iki1uc <entertrainer@web.de>
2026-08-17 02:33:00 +0200

    Update KASSE.js

--- RESPO /KASSE.js
+++ RESPO /KASSE.js
@@ -1,1 +1,11 @@
-
+// KASSE final — Sammelimpuls AUSGABE
+export function KASSE() {
+    return {
+        move: "KASSE",
+        type: "collect-output",
+        height: "mystery-mid",
+        width: "mystery-bundle",
+        depth: "mystery-stabilize",
+        stable: true
+    };
+}
