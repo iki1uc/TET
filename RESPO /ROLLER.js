67e0f2599771d05aa0b811626bc4a4468a9a9020
iki1uc <entertrainer@web.de>
2026-08-17 02:33:21 +0200

    Update ROLLER.js

--- RESPO /ROLLER.js
+++ RESPO /ROLLER.js
@@ -1,1 +1,15 @@
+// TET final — trägt ESSO, ROLLER, KASSE
 
+import { ESSO } from "./ESSO.js";
+import { ROLLER } from "./ROLLER.js";
+import { KASSE } from "./KASSE.js";
+
+export const TET = {
+    run() {
+        return [
+            ESSO(),
+            ROLLER(),
+            KASSE()
+        ];
+    }
+};
